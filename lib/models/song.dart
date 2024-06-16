9b1a678c196b765358225cd7e78e29d0fffad160
Youssab El Khanagry <subamichael28@gmail.com>
2024-06-16 17:26:54 +0300

    Added song model

---
+++ lib/models/song.dart
@@ -0,0 +1,12 @@
+class Song {
+  String songName;
+  String artistName;
+  String albumArtImagePath;
+  String audioPath;
+
+  Song(
+      {required this.songName,
+      required this.artistName,
+      required this.albumArtImagePath,
+      required this.audioPath});
+}
