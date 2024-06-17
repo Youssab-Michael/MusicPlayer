194985882fe17fa123b781010db8fe93d20b3d16
Youssab El Khanagry <subamichael28@gmail.com>
2024-06-17 23:58:31 +0300

    imageBox modified

--- lib/components/image_box.dart
+++ lib/components/image_box.dart
@@ -1,35 +1,34 @@
 import 'package:flutter/material.dart';
-import 'package:musicplayer/components/gradient_container.dart';
 
 class ImageBox extends StatelessWidget {
-  final String image;
+  final Widget? child;
 
   const ImageBox({
     super.key,
-    required this.image,
+    required this.child,
   });
 
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-        color: Theme.of(context).colorScheme.surface,
+        color: Theme.of(context).colorScheme.secondary,
         borderRadius: BorderRadius.circular(12),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey.shade500,
-            blurRadius: 15,
-            offset: const Offset(4, 4)
-          ),
-          const BoxShadow(
-            color: Colors.white,
-            blurRadius: 15,
-            offset: Offset(-4, -4)
-          )
-        ]
+        // boxShadow: [
+        //   BoxShadow(
+        //     color: Colors.grey.shade500,
+        //     blurRadius: 15,
+        //     offset: const Offset(4, 4)
+        //   ),
+        //   const BoxShadow(
+        //     color: Colors.white,
+        //     blurRadius: 15,
+        //     offset: Offset(-4, -4)
+        //   )
+        // ]
       ),
       padding: const EdgeInsets.all(12),
-      child: GradientContainer(imagePath: image),
+      child: child,
     );
   }
 }
