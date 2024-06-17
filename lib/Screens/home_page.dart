7ae2622321456608830efe50180de4fa976a52e7
Youssab El Khanagry <subamichael28@gmail.com>
2024-06-17 18:23:29 +0300

    Modifying PlaylistProvider,HomePage

--- lib/Screens/home_page.dart
+++ lib/Screens/home_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:musicplayer/Screens/song_page.dart';
 import 'package:musicplayer/components/myDrawer.dart';
+import 'package:musicplayer/models/playlist_provider.dart';
+import 'package:musicplayer/models/song.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -9,12 +13,49 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late final dynamic playlistProvider;
+
+  @override
+  void initState() {
+    super.initState();
+    playlistProvider = Provider.of<PlaylistProvider>(context, listen: false);
+  }
+
+  void goToSong(int songIndex) {
+    playlistProvider.currentPlayingIndex = songIndex;
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => const SongPage(),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.surface,
-      appBar: AppBar(title: const Text("Music Player"),),
+      appBar: AppBar(
+        title: const Text("Music Player"),
+      ),
       drawer: const MyDrawer(),
+      body: Consumer<PlaylistProvider>(
+        builder: (context, value, child) {
+          final List<Song> playlist = value.playlist;
+          return ListView.builder(
+            itemCount: playlist.length,
+            itemBuilder: (context, index) {
+              final Song song = playlist[index];
+              return ListTile(
+                title: Text(song.songName),
+                subtitle: Text(song.artistName),
+                leading: Image.asset(song.albumArtImagePath),
+                onTap: () => goToSong(index),
+              );
+            },
+          );
+        },
+      ),
     );
   }
 }
--- lib/models/playlist_provider.dart
+++ lib/models/playlist_provider.dart
@@ -1,5 +1,36 @@
 import 'package:flutter/cupertino.dart';
+import 'package:musicplayer/models/song.dart';
 
-class playlistProvider extends ChangeNotifier{
-  
+class PlaylistProvider extends ChangeNotifier {
+  final List<Song> _playlist = [
+    Song(
+        songName: "Champions League",
+        artistName: "Marwan Moussa",
+        albumArtImagePath: "assets/images/Champions League.jpeg",
+        audioPath: "assets/audio/CHAMPIONS LEAGUE.mp3"),
+    Song(
+        songName: "Lelly Yah",
+        artistName: "Marwan Pablo",
+        albumArtImagePath: "assets/images/Lelly Yah.jpeg",
+        audioPath: "assets/audio/Lelly Yah - Marwan Pablo.mp3"),
+    Song(
+        songName: "Messi",
+        artistName: "Abo El Anwar",
+        albumArtImagePath: "assets/images/Messi.jpeg",
+        audioPath: "assets/audio/Abo El Anwar - Messi.mp3"),
+  ];
+
+  int? _currentPlayingIndex;
+
+  // getter
+  List<Song> get playlist => _playlist;
+
+  int? get currentPlayingIndex => _currentPlayingIndex;
+
+  // setter
+  set currentPlayingIndex(int? newIndex) {
+    _currentPlayingIndex = newIndex;
+
+    notifyListeners();
+  }
 }
