a48788a3dd8cf601de8580d660ad4ae190214c75
Youssab El Khanagry <subamichael28@gmail.com>
2024-06-18 12:10:07 +0300

    SongPage Modified

--- lib/Screens/song_page.dart
+++ lib/Screens/song_page.dart
@@ -9,133 +9,146 @@ class SongPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Consumer<PlaylistProvider>(
-      builder: (context, value, child) => Scaffold(
-        backgroundColor: Theme.of(context).colorScheme.surface,
-        appBar: AppBar(
-          title: const Text("Playlist"),
-          actions: [
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(Icons.menu),
-            ),
-          ],
-        ),
-        body: SafeArea(
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: Column(
-                  children: [
-                    ImageBox(
-                      child: Column(
-                        children: [
-                          ClipRRect(
-                            borderRadius: BorderRadius.circular(12),
-                            child: Image.asset(
-                              "assets/images/Champions League.jpeg",
-                            ),
-                          ),
-                          const Padding(
-                            padding: EdgeInsets.all(15.0),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                      "Champions League",
-                                      style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 20,
-                                      ),
-                                    ),
-                                    Text("Marwan Moussa"),
-                                  ],
-                                ),
-                                Icon(
-                                  Icons.favorite,
-                                  color: Colors.red,
-                                )
-                              ],
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 25,
-                    ),
-                    Column(
-                      children: [
-                        const Padding(
-                          padding: EdgeInsets.all(24.0),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text("0:00"),
-                              Icon(Icons.shuffle),
-                              Icon(Icons.repeat),
-                              Text("0:00"),
-                            ],
-                          ),
-                        ),
-                        SliderTheme(
-                          data: SliderTheme.of(context).copyWith(
-                            thumbShape:
-                                const RoundSliderThumbShape(enabledThumbRadius: 0),
-                          ),
-                          child: Slider(
-                            min: 1,
-                            max: 100,
-                            value: 50,
-                            onChanged: (value) {},
-                          ),
-                        ),
-                        const SizedBox(
-                          height: 25,
-                        ),
-                        Row(
-                          children: [
-                            Expanded(
-                              child: GestureDetector(
-                                onTap: () {},
-                                child: const ImageBox(
-                                  child: Icon(Icons.skip_previous),
-                                ),
-                              ),
-                            ),
-                            const SizedBox(width: 25,),
-                            Expanded(
-                              flex: 2,
-                              child: GestureDetector(
-                                onTap: () {},
-                                child: const ImageBox(
-                                  child: Icon(Icons.play_arrow),
-                                ),
-                              ),
-                            ),
-                            const SizedBox(width: 25,),
-                            Expanded(
-                              child: GestureDetector(
-                                onTap: () {},
-                                child: const ImageBox(
-                                  child: Icon(Icons.skip_next),
-                                ),
-                              ),
-                            ),
-                          ],
-                        )
-                      ],
-                    )
-                  ],
-                ),
+      builder: (context, value, child) {
+
+        final playlist = value.playlist;
+
+        final currentSong = playlist[value.currentPlayingIndex ?? 0];
+
+        return Scaffold(
+          backgroundColor: Theme.of(context).colorScheme.surface,
+          appBar: AppBar(
+            title: const Text("Playlist"),
+            actions: [
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.menu),
               ),
             ],
           ),
-        ),
-      ),
+          body: SafeArea(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Column(
+                    children: [
+                      ImageBox(
+                        child: Column(
+                          children: [
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(12),
+                              child: Image.asset(
+                                currentSong.albumArtImagePath,
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(15.0),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        currentSong.songName,
+                                        style: const TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 20,
+                                        ),
+                                      ),
+                                      Text(currentSong.artistName),
+                                    ],
+                                  ),
+                                  const Icon(
+                                    Icons.favorite,
+                                    color: Colors.red,
+                                  )
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 25,
+                      ),
+                      Column(
+                        children: [
+                          const Padding(
+                            padding: EdgeInsets.all(24.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text("0:00"),
+                                Icon(Icons.shuffle),
+                                Icon(Icons.repeat),
+                                Text("0:00"),
+                              ],
+                            ),
+                          ),
+                          SliderTheme(
+                            data: SliderTheme.of(context).copyWith(
+                              thumbShape: const RoundSliderThumbShape(
+                                  enabledThumbRadius: 0),
+                            ),
+                            child: Slider(
+                              min: 1,
+                              max: 100,
+                              value: 50,
+                              onChanged: (value) {},
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 25,
+                          ),
+                          Row(
+                            children: [
+                              Expanded(
+                                child: GestureDetector(
+                                  onTap: () {},
+                                  child: const ImageBox(
+                                    child: Icon(Icons.skip_previous),
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(
+                                width: 25,
+                              ),
+                              Expanded(
+                                flex: 2,
+                                child: GestureDetector(
+                                  onTap: () {},
+                                  child: const ImageBox(
+                                    child: Icon(Icons.play_arrow),
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(
+                                width: 25,
+                              ),
+                              Expanded(
+                                child: GestureDetector(
+                                  onTap: () {},
+                                  child: const ImageBox(
+                                    child: Icon(Icons.skip_next),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
